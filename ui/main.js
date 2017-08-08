8e8acb731232687f4cc9467c0994310f193d520d
vj98 <bhativijay17@gmail.com>
2017-08-08 22:01:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 //Have the image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginleft = '100px';
 };
