95813b0b59dc560f87a9baa184037eb43a389f59
vj98 <bhativijay17@gmail.com>
2017-08-08 22:50:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function(req, res) {
     counter = counter + 1;
-    res.send(counter.toString());
+    res.send(counter.toString()); //always send counter in string
 });
 
 app.get('/:articlename', function (req, res) {
