8b4e2401c68480e8dcfe407e366ed81524d24845
vj98 <bhativijay17@gmail.com>
2017-08-08 21:56:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+//Changing text of the main-text div
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'New Value';
+
+//Have the image
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginleft = '100px';
+};
+
