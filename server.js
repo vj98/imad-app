ea7fe3b52401aab2e10e8af5825762277e5391d7
vj98 <bhativijay17@gmail.com>
2017-08-08 22:48:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articlename', function (req, res) {
     var articlename = req.params.articlename;
     res.send(createtemplate(articles[articlename]));
