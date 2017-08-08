4e49b410a73e3e3779391491368eb28b6a094cad
vj98 <bhativijay17@gmail.com>
2017-08-08 23:17:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,119 +1,15 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-var articles = {
-    'article-one' :  {
-        title: "Article one | Vijay Bhati",
-        heading: "Article one",
-        date: "7 Aug 2017",
-        content: `
-            <p>
-                This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.  This is the content for my first article.
-            </p> `
-    },
-    
-    'article-two' :  {
-        title: "Article two | Vijay Bhati",
-        heading: "Article two",
-        date: "7 Aug 2017",
-        content: `
-            <p>
-                This is the content for my second article.
-            </p>` 
-    },
-    
-    'article-three' :  {
-        title: "Article two | Vijay Bhati",
-        heading: "Article three",
-        date: "7 Aug 2017",
-        content: `
-            <p>
-                This is the content for my third article.
-            </p> `
-    }
-};
-
-
-function createtemplate(data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    
-    var htmltemplate = `
-        <html>
-            <head>
-                <title> 
-                ${title}
-                </title>
-                <meta name = "viewport" content = "width = device-width, initial-scale = 1" />
-                <link href="/ui/style.css" rel="stylesheet" />
-            </head>
-            <body>
-                <div class = "container">
-                    <div>
-                        <a href = "/">Home</a>
-                    </div>
-                    <hr/> <!--Horizontal rule -->
-                    <h3>
-                        ${heading}
-                    </h3>
-                    <div>
-                       ${date}
-                    </div>
-                    <div>
-                       ${content}
-                    </div>
-                </div>
-                
-            </body>
-        </html>
-        `;
-        return htmltemplate;
-}
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
+//Counter Code
+var button = document.getElementById('counter');
 var counter = 0;
-app.get('/counter', function(req, res) {
-    counter = counter + 1;
-    res.send(counter.toString()); //always send counter in string
-});
 
-app.get('/:articlename', function (req, res) {
-    var articlename = req.params.articlename;
-    res.send(createtemplate(articles[articlename]));
-});
-
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+button.onclick = function() {
+  
+  //Make a repeat to the counter endpoint
+  
+  //Capture response and store it in a variable
+  
+  //Render the variable in correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
