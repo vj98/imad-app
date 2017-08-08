2f75ce582fb408c1cc0c4d3c62479bb566d462f3
vj98 <bhativijay17@gmail.com>
2017-08-08 22:07:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,13 @@ element.innerHTML = 'New Value';
 
 //Have the image
 var img = document.getElementById('madi');
+
+var marginleft = 0;
+function moveRight () {
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + 'px';
+}
+
 img.onclick = function() {
     img.style.marginleft = '100px';
 };
