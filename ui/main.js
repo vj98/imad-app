1aa7020149f2d83b31b38092e835a1970c822aca
vj98 <bhativijay17@gmail.com>
2017-08-12 10:53:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,21 @@ button.onclick = function() {
   request.open('GET', 'http://bhativijay17.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //Make requesst to the server and send the name
+    
+    //Capture a list  of names and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i = 0; i < names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ui.innerHTML = list;
+};
+};
