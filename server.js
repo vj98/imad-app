bdeda627cc23f3edbcb5dca4a71ff33a21242da2
vj98 <bhativijay17@gmail.com>
2017-08-19 09:46:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'vj98',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: db-bhativijay17-92436
+    password: 'db-bhativijay17-92436'
 };
 
 var app = express();
